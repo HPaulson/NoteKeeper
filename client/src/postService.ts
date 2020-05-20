9c96736c0726d7052e459ab1d6367489ae3f86e6
Hunter Paulson <hpaulson@smc.wtf>
2020-05-20 00:57:16 -0400

    Update and rename postService.js to postService.ts

--- client/src/postService.ts
+++ client/src/postService.ts
@@ -1,25 +1,18 @@
 import axios from 'axios'
 
-const url = 'http://localhost:80/api/posts/'
+const url = 'http://localhost:6969/api/posts/'
 
 class postService {
     
-    static getPosts() {
-        return new Promise(async (res, rej) => {
-            try {
+    static async getPosts() {
                 const resolve = await axios.get(url)
                 const data = resolve.data
                 console.log(data.data)
-                res(data.data.map((data) => ({
+                return data.data.map((data) => ({
                     ...data,
                     createdAt: new Date(data.createdAt)
-                })))
+                }))
             }
-            catch (e) {
-                rej(e)
-            }
-        })
-    }
 
     static makePost(text) {
         return axios.post(url, {
